9c30c2466f4aaad686ae3cfb88e64bbfdaf29e6e
wooldridge <mike@hellion.org>
2021-11-22 01:18:43 -0800

    Fake search API with Mockaroo data
    Fake faceted search by filtering dataset.

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,14 +1,41 @@
 // import axios from "axios";
+import persons from "../mocks/persons.json";
 import {searchResults} from "../mocks/results";
 import {summary} from "../mocks/summary";
 import {saved} from "../mocks/saved";
-import {details} from "../mocks/details";
+import {detail} from "../mocks/detail";
+import _ from "lodash";
 
 // export const getSearchResults = async (query) => { // TODO
 export const getSearchResults = (query) => {
   // return await axios.get(`/api/searchResults`); // TODO
-  console.log("getSearchResults", query, searchResults);
-  return searchResults;
+  console.log("getSearchResults", query);
+  const results = _.clone(searchResults);
+  results["start"] = query.start;
+  results["pageLength"] = query.pageLength;
+  let personsSlice = persons.slice((query.start - 1), (query.start + query.pageLength -1));
+  let fsKeys = Object.keys(query.facetStrings);
+  let newArr;
+  // TODO Fake faceted search for now...
+  fsKeys.forEach(key => {
+    if (key === "Status") {
+      newArr = personsSlice.filter(p => {
+        return query.facetStrings[key].includes(p.entityInstance.status);
+      })
+      personsSlice = newArr;
+    }
+    if (key === "Sources") {
+      newArr = personsSlice.filter(p => {
+        let intersected = _.intersection(query.facetStrings[key], p.entityInstance.sources);
+        return intersected.length > 0;
+      })
+      personsSlice = newArr;
+    }
+  });
+  results["results"] = personsSlice;
+  results["returned"] = personsSlice.length;
+  results["total"] = persons.length;
+  return results;
 };
 
 // export const getSummary = async (opts) => { // TODO
@@ -28,6 +55,11 @@ export const getSaved = (opts) => {
 // export const getDetail = async (opts) => { // TODO
 export const getDetail = (opts) => {
   // return await axios.get(`/api/saved`); // TODO
-  console.log("getDetail", opts, details);
-  return details[opts];
+  const result = _.clone(detail);
+  const person = persons.find(p => p.entityInstance.personId === parseInt(opts));
+  if (person !== undefined) {
+    result["docUri"] = person["uri"];
+    result["entityInstanceProperties"] = person.entityInstance;
+  }
+  return result;
 };
--- src/mocks/results.ts
+++ src/mocks/results.ts
@@ -1,174 +1,9 @@
 export const searchResults = {
   "snippet-format": "snippet",
-  "total": 3,
-  "start": 1,
-  "page-length": 10,
-  "results": [
-    {
-      "index": 1,
-      "uri": "/Person/Person1.json",
-      "path": "fn:doc(\"/Person/Person1.json\")",
-      "score": 0,
-      "confidence": 0,
-      "fitness": 0,
-      "href": "/v1/documents?uri=%2FPerson%2FPerson1.json",
-      "mimetype": "application/json",
-      "format": "json",
-      "matches": [
-        {
-          "path": "fn:doc(\"/Person/Person1.json\")/envelope/headers/array-node(\"sources\")/object-node()[2]/text(\"datahubSourceType\")",
-          "match-text": [
-            {
-              "highlight": "JSON"
-            }
-          ]
-        }
-      ],
-      "primaryKey": {
-        "propertyPath": "customerId",
-        "propertyValue": 105
-      },
-      "identifier": {
-        "propertyPath": "identifier",
-        "propertyValue": 105
-      },
-      "entityName": "Customer",
-      "createdOn": "2021-10-28T22:47:18.695808-07:00",
-      "createdBy": "admin",
-      "entityInstance": {
-        "customerId": 105,
-        "name": [
-          "Hollan Welles",
-          "Holland Wells"
-        ],
-        "email": "hwelles@example.org",
-        "phone": "415-555-1234",
-        "image": "https://cdn1.marklogic.com/wp-content/uploads/2021/07/chuck-hollis.jpeg",
-        "address": {
-          "street": "Sunnyside Avenue",
-          "city": "Brutus",
-          "state": "Wisconsin",
-          "zip": {
-            "fiveDigit": "30706",
-            "plusFour": "8854"
-          }
-        },
-        "status": "Active",
-        "ssn": "123-45-6789",
-        "sources": [
-          "USA Today",
-          "New York Times"
-        ],
-      }
-    },
-    {
-      "index": 2,
-      "uri": "/Person/Person2.json",
-      "path": "fn:doc(\"/Person/Person2.json\")",
-      "score": 0,
-      "confidence": 0,
-      "fitness": 0,
-      "href": "/v1/documents?uri=%2FPerson%2FPerson2.json",
-      "mimetype": "application/json",
-      "format": "json",
-      "matches": [
-        {
-          "path": "fn:doc(\"/Person/Person2.json\")/envelope/headers/array-node(\"sources\")/object-node()[2]/text(\"datahubSourceType\")",
-          "match-text": [
-            {
-              "highlight": "JSON"
-            }
-          ]
-        }
-      ],
-      "primaryKey": {
-        "propertyPath": "customerId",
-        "propertyValue": 101
-      },
-      "identifier": {
-        "propertyPath": "identifier",
-        "propertyValue": 101
-      },
-      "entityName": "Customer",
-      "createdOn": "2021-10-28T22:47:18.695808-07:00",
-      "createdBy": "admin",
-      "entityInstance": {
-        "customerId": 101,
-        "name": "Carmella Hardin",
-        "email": "carmella@example.com",
-        "phone": "510-555-4321",
-        "image": "https://cdn1.marklogic.com/wp-content/uploads/2018/02/trinh-lieu-profile.jpg",
-        "address": {
-          "street": "Anna Court",
-          "city": "Stewart",
-          "state": "Kansas",
-          "zip": {
-            "fiveDigit": "66601",
-            "plusFour": "6783"
-          }
-        },
-        "status": "Active",
-        "ssn": "111-11-1111",
-        "sources": [
-          "New York Times"
-        ],
-      }
-    },
-    {
-      "index": 3,
-      "uri": "/Person/Person7.json",
-      "path": "fn:doc(\"/Person/Person7.json\")",
-      "score": 0,
-      "confidence": 0,
-      "fitness": 0,
-      "href": "/v1/documents?uri=%2FPerson%2FPerson7.json",
-      "mimetype": "application/json",
-      "format": "json",
-      "matches": [
-        {
-          "path": "fn:doc(\"/Person/Person7.json\")/envelope/headers/array-node(\"sources\")/object-node()[2]/text(\"datahubSourceType\")",
-          "match-text": [
-            {
-              "highlight": "JSON"
-            }
-          ]
-        }
-      ],
-      "primaryKey": {
-        "propertyPath": "customerId",
-        "propertyValue": 102
-      },
-      "identifier": {
-        "propertyPath": "identifier",
-        "propertyValue": 102
-      },
-      "entityName": "Customer",
-      "createdOn": "2021-10-28T22:47:18.695808-07:00",
-      "createdBy": "admin",
-      "entityInstance": {
-        "customerId": 102,
-        "name": "Adams Cole",
-        "email": "ac@example.net",
-        "phone": "925-555-9876",
-        "image": "https://cdn1.marklogic.com/wp-content/uploads/2021/02/1612313387205.jpeg",
-        "address": {
-          "street": "Varick Avenue",
-          "city": "Mooresburg",
-          "state": "Delaware",
-          "zip": {
-            "fiveDigit": "17654",
-            "plusFour": "1292"
-          }
-        },
-        "status": "Inactive",
-        "ssn": "222-22-2222",
-        "sources": [
-          "Chicago Tibune",
-          "USA Today"
-        ]
-      }
-    }
-  ],
+  "total": 3, // Filled in dynamically
+  "start": 1, // Filled in dynamically
+  "page-length": 10, // Filled in dynamically
+  "results": [], // Filled in dynamically
   "facets": {
     "Entities": {
       "type": "xs:string",
@@ -207,6 +42,21 @@ export const searchResults = {
           "name": "Chicago Tribune",
           "count": 43276,
           "value": "Chicago Tribune"
+        },
+        {
+          "name": "Wall Street Journal",
+          "count": 12983,
+          "value": "Wall Street Journal"
+        },
+        {
+          "name": "Los Angeles Times",
+          "count": 11554,
+          "value": "Los Angeles Times"
+        },
+        {
+          "name": "Washington Post",
+          "count": 8306,
+          "value": "Washington Post"
         }
       ]
     },
