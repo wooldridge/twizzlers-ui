c9809a0c0d3a68b6be73fc76e3ae2003d64705a6
wooldridge <mike@hellion.org>
2021-12-10 19:19:29 -0800

    Start error-handling wrapper
    Centralize REST error handling.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import SearchProvider from "./store/SearchContext";
 import DetailProvider from "./store/DetailContext";
+import ErrorHandler from "./store/ErrorHandler";
 import Dashboard from "./views/Dashboard";
 import Detail from "./views/Detail";
 import Search from "./views/Search";
@@ -13,18 +14,20 @@ type Props = {};
 const App: React.FC<Props> = (props) => {
 
   return (
-    <SearchProvider>
-      <DetailProvider>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/search" element={<Search />} />
-            <Route path="/detail/:id" element={<Detail />} />
-          </Routes>
-        </main>
-      </DetailProvider>
-    </SearchProvider>
+    <ErrorHandler>
+      <SearchProvider>
+        <DetailProvider>
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/search" element={<Search />} />
+              <Route path="/detail/:id" element={<Detail />} />
+            </Routes>
+          </main>
+        </DetailProvider>
+      </SearchProvider>
+    </ErrorHandler>
   );
 
 };
